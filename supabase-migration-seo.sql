8cb49edeb284d639fae9491f699de950b5adce03
jon-vdb <jonvdberg8@gmail.com>
2026-04-11 19:15:37 +0200

    delete

--- supabase-migration-seo.sql
+++
@@ -1,4 +0,0 @@
--- Add author and SEO fields to posts table
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS author_name text DEFAULT '';
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS meta_description text DEFAULT '';
-ALTER TABLE posts ADD COLUMN IF NOT EXISTS meta_keywords text DEFAULT '';
